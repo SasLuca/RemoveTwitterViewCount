db3c9135900fbf157e9f52345553a988af5c566a
SasLuca <sas.luca.al@gmail.com>
2023-01-18 13:22:25 +0200

    Updated for Jan 17th Twitter Update

--- content.js
+++ content.js
@@ -3,8 +3,8 @@ const isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
 if (!isFirefox)
 {
     const style = `<style>
-    div[id^='id__'].css-1dbjc4n.r-1ta3fxp.r-18u37iz.r-1wtj0ep.r-1s2bzr4.r-1mdbhws > div:first-child, 
-    .css-1dbjc4n.r-2sztyj.r-1efd50x.r-5kkj8d.r-13awgt0.r-18u37iz.r-tzz3ar.r-s1qlax.r-1yzf0co > div:first-child,
+    div[id^='id__'].css-1dbjc4n.r-1ta3fxp.r-18u37iz.r-1wtj0ep.r-1s2bzr4.r-1mdbhws > div:nth-child(4), 
+    .css-1dbjc4n.r-2sztyj.r-1efd50x.r-5kkj8d.r-13awgt0.r-18u37iz.r-tzz3ar.r-s1qlax.r-1yzf0co > div:nth-child(4),
     .r-1h0z5md:has([href$="/analytics"]),
     .r-1yzf0co .css-1dbjc4n:has([href$="/analytics"])
     {
@@ -18,7 +18,7 @@ if (!isFirefox)
 if (isFirefox)
 {
     const style = `<style>
-    div[id^='id__'].css-1dbjc4n.r-1ta3fxp.r-18u37iz.r-1wtj0ep.r-1s2bzr4.r-1mdbhws > div:first-child, .css-1dbjc4n.r-2sztyj.r-1efd50x.r-5kkj8d.r-13awgt0.r-18u37iz.r-tzz3ar.r-s1qlax.r-1yzf0co > div:first-child
+    div[id^='id__'].css-1dbjc4n.r-1ta3fxp.r-18u37iz.r-1wtj0ep.r-1s2bzr4.r-1mdbhws > div:nth-child(4), .css-1dbjc4n.r-2sztyj.r-1efd50x.r-5kkj8d.r-13awgt0.r-18u37iz.r-tzz3ar.r-s1qlax.r-1yzf0co > div:nth-child(4)
     {
         display: none !important;
     }
